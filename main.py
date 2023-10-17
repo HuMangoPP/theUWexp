fe00088a1108d935bd1a1d06c5ebda67387a31b2
Mike <supahotboi69@gmail.com>
2023-10-17 09:38:06 -0400

    removed some comments

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
 #!/usr/bin/env python
-
-#  IDEAS
-# all -> slander uoft, depression
-# the avg eng student -> rock climb, gaming/valorant/league, leetcode grind, flexing difficulty/homework volume, eng incompetency
-# the avg cs/math student -> pink-tie weapon, rock climb, gaming/valorant/league, leetcode grind, co-op wage higher than rest, no friends
-# the avg sci student -> labs/lab equipment (goggles), co-op opportunities = none, research = no wage, 
-# the avg art student -> no co-op opportunities, chad/don't give a shit about stem, 
-# the avg env student -> leaf/grass weapon (ranged), reuseable cup for coffee, hipster af, 
-# the avg health student -> med scrubs weapon, chad/fit as fuck, sports/flex on eng and math, 
-# most humble waterloo student -> final boss, faang co-op, 4.0 gpa, flex on everyone,
-# references to co-op, courses, etc
-# mr goose -> sub boss
-
-# there can be a story associated with the game, where an incoming student encounters all of these people (they get stronger as they get into higher years)
-# as the MC defeats other students, they level up into the next year; the MC must defeat the most humble waterloo student as a final boss to graduate
-# the end game is that the MC graduates with their degree and co-op experience
-# tones: lighthearted, humour, etc
-
 from src.client import Client
 import sys
 
